9a208dfc3ec0c3828dc33743917bbbeca8994dd1
sebastienheyd@gmail.com <sebastienheyd@gmail.com@063b2e71-347a-c81b-b46b-1f68757473af>
2011-11-28 10:25:17 +0000

    small optimizations in Security and human readable password generation

--- trunk/oxygen/core/security.class.php
+++ trunk/oxygen/core/security.class.php
@@ -35,38 +35,32 @@ class Security
      * AES Decrypt a string, very useful for reading encrypted passwords from database per example
      * 
      * @param string $val   The string to decrypt
-     * @param string $ky    (optionnal) The key to use for decryption, by default use the application key
+     * @param string $key   (optionnal) The key to use for decryption, by default use the application key
      * @return string       The decrypted value or crypted if key is incorrect
      */
-    public static function aesDecrypt($val,$ky = null)
+    public static function aesDecrypt($val, $key = null)
     {
-        if(is_null($ky)) $ky = self::getKey();
+        if(is_null($key)) $key = self::getKey();
         $val = base64_decode($val);
-        $key="\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
-        for($a=0;$a<strlen($ky);$a++)
-          $key[$a%16]=chr(ord($key[$a%16]) ^ ord($ky[$a]));
-        $mode = MCRYPT_MODE_ECB;
-        $enc = MCRYPT_RIJNDAEL_128;
-        $dec = @mcrypt_decrypt($enc, $key, $val, $mode, @mcrypt_create_iv( @mcrypt_get_iv_size($enc, $mode), MCRYPT_DEV_URANDOM ) );
-        return rtrim($dec,(( ord(substr($dec,strlen($dec)-1,1))>=0 and ord(substr($dec, strlen($dec)-1,1))<=16)? chr(ord( substr($dec,strlen($dec)-1,1))):null));
+        $k = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
+        for($a = 0; $a < strlen($key); $a++) $k[$a%16] = chr(ord($k[$a%16]) ^ ord($key[$a]));          
+        $dec = @mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $k, $val, MCRYPT_MODE_ECB, @mcrypt_create_iv( @mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_ECB), MCRYPT_DEV_URANDOM ) );
+        return rtrim($dec,(( ord(substr($dec, strlen($dec)-1,1)) >= 0 and ord(substr($dec, strlen($dec)-1, 1)) <= 16) ? chr(ord( substr($dec,strlen($dec)-1, 1))) : null));
     }
 
     /**
      * AES Encrypt a string. Very useful to save passwords in the database per example
      * 
      * @param string $val   The string to encrypt
-     * @param string $ky    (optionnal) The key to use for encryption
+     * @param string $key   (optionnal) The key to use for encryption
      * @return string       The encrypted string in base64 (url compatible)
      */
-    public static function aesEncrypt($val,$ky = null)
+    public static function aesEncrypt($val, $key = null)
     {
-        if(is_null($ky)) $ky = self::getKey();
-        $key="\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
-        for($a=0;$a<strlen($ky);$a++)
-          $key[$a%16]=chr(ord($key[$a%16]) ^ ord($ky[$a]));
-        $mode=MCRYPT_MODE_ECB;
-        $enc=MCRYPT_RIJNDAEL_128;
-        $val=str_pad($val, (16*(floor(strlen($val) / 16)+(strlen($val) % 16==0?2:1))), chr(16-(strlen($val) % 16)));
-        return base64_encode(mcrypt_encrypt($enc, $key, $val, $mode, mcrypt_create_iv( mcrypt_get_iv_size($enc, $mode), MCRYPT_DEV_URANDOM)));
+        if(is_null($key)) $key = self::getKey();
+        $k="\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
+        for($a = 0; $a < strlen($key); $a++) $k[$a%16] = chr(ord($k[$a%16]) ^ ord($key[$a]));
+        $val = str_pad($val, (16*(floor(strlen($val) / 16) + (strlen($val) % 16 == 0 ? 2 : 1))), chr(16-(strlen($val) % 16)));
+        return base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $k, $val, MCRYPT_MODE_ECB, mcrypt_create_iv( mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_ECB), MCRYPT_DEV_URANDOM)));
     }     
 }
