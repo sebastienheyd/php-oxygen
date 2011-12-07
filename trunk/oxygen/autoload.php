3f469fbe1762c7e2f1e205756612df19fe4fb8a4
sebastien.heyd@gmail.com <sebastien.heyd@gmail.com@063b2e71-347a-c81b-b46b-1f68757473af>
2011-12-07 16:40:38 +0000

    Smarty 3.1.6 + autoload modification for it

--- trunk/oxygen/autoload.php
+++ trunk/oxygen/autoload.php
@@ -11,46 +11,32 @@
  * @package     PHP Oxygen
  */
 
+define(SMARTY_SPL_AUTOLOAD, 1);
+define(PS, PATH_SEPARATOR);
 
-// Include core path
-set_include_path(get_include_path().PATH_SEPARATOR.FW_DIR.DS.'core'.PATH_SEPARATOR.FW_DIR.DS.'lib'.PATH_SEPARATOR.PROJECT_DIR.DS.'model');
+// include paths
+set_include_path(get_include_path().PS.
+                 FW_DIR.DS.'core'.PS.
+                 FW_DIR.DS.'lib'.PS.
+                 FW_DIR.DS.'lib'.DS.'vendor'.DS.'smarty'.DS.'sysplugins'.PS.
+                 PROJECT_DIR.DS.'model');
 
-// Define file extensions to get
-spl_autoload_extensions('.class.php');
+// define file extensions to get
+spl_autoload_extensions('.class.php,.php');
 
-// First try to load from core
+// first try to load from included paths
 spl_autoload_register();
 
-// Or load from defined autoload
-spl_autoload_register('Autoload::load');
+// or load from autoload method
+spl_autoload_register('autoload');
 
-class Autoload
+/**
+ * Magic __autoload method
+ * See : http://www.php.net/autoload
+ * 
+ * @param string $class_name    The class name to get
+ */
+function autoload($className)
 {
-    /**
-     * Magic __autoload method
-     * See : http://www.php.net/autoload
-     * 
-     * @param string $class_name    The class name to get
-     */
-    public static function load($className)
-    {
-        // specific case for Smarty 3
-        if(strstr($className, 'Smarty'))
-        {
-            $_class = strtolower($className);
-            if (substr($_class, 0, 16) === 'smarty_internal_' || $_class == 'smarty_security') require_once FW_DIR.DS."lib".DS.'vendor'.DS."smarty".DS.'sysplugins'.DS. $_class . '.php';
-            return;
-        }
-
-        $path = class_file_path($className);
-
-        // check if file exists
-        if(!$path) throw new Exception('Cannot load file for class '.$className);
-
-        //if true load the file
-        require_once($path);
-    }
-    
+    if($path = class_file_path($className)) require_once($path);
 }
-
-
