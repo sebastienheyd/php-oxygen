cca3796120c37a428c85abf75d8f885bfe72822d
sebastienheyd@gmail.com <sebastienheyd@gmail.com@063b2e71-347a-c81b-b46b-1f68757473af>
2012-06-15 14:52:01 +0000

    Added cookie handler

---
+++ trunk/oxygen/core/cookie.class.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * This file is part of the PHP Oxygen package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright   Copyright (c) 2011 Sébastien HEYD <sheyd@php-oxygen.com>
+ * @author      Sébastien HEYD <sheyd@php-oxygen.com>
+ * @package     PHP Oxygen
+ */
+
+class Cookie 
+{
+    public static function expire($value, $unit = 's')
+    {
+        $units = array('s' => 1, 'm' => 60, 'h' => 3600, 'd' => 86400);
+        if(!in_array($unit, array_keys($units))) throw new InvalidArgumentException('Unknown unit : '.$unit);
+        return $value * $units[$unit];        
+    }
+    
+    /**
+     *
+     * @param type $name
+     * @return boolean 
+     */
+    public static function get($name)
+    {
+        if(isset($_COOKIE[$name]))
+        {
+            $value = Security::decode($_COOKIE[$name]);
+            if($v = unserialize($value)) $value = $v;
+            return $value;
+        }
+        return false;
+    }
+    
+    /**
+     *
+     * @param type $name
+     * @param type $value
+     * @param type $lifetime
+     * @param type $path
+     * @param type $domain
+     * @param type $secure
+     * @param type $httponly
+     * @return type
+     * @throws OverflowException 
+     */
+    public static function set($name, $value = '', $lifetime = 3600, $path = null, $domain = null, $secure = null, $httponly = null)
+    {
+        // false value will delete the cookie so we force the value as an integer
+        if($value === true || $value === false) $value = (int) $value;
+        if($value === null) return setrawcookie($name, false, time() - 3600);
+        
+        if(is_array($value) && !empty($value)) $value = serialize($value);
+        if(is_string($value) && $value !== '')
+        {
+            $value = Security::encode($value);                
+            if ( strlen( $value ) > ( 4 * 1024 ) ) throw new OverflowException("The cookie $name exceeds the maximum cookie size. Some data may be lost");            
+        }
+        
+        $_COOKIE[$name] = $value;        
+        return setrawcookie($name, $value, time() + $lifetime, $path, $domain, $secure, $httponly);
+    }
+    
+    /**
+     *
+     * @param type $name
+     * @return type 
+     */
+    public static function delete($name)
+    {
+        unset($_COOKIE[$name]);
+        return self::set($name, null);
+    }
+}
