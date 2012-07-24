89d8d7279bbb6f1064c58a66216f3003a2754bfd
sebastien.heyd@gmail.com <sebastien.heyd@gmail.com@063b2e71-347a-c81b-b46b-1f68757473af>
2012-07-24 12:35:26 +0000

    platforms as an array

---
+++ trunk/oxygen/lib/data/platforms.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * This file is part of the PHP Oxygen package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright   Copyright (c) 2011 Sébastien HEYD <sheyd@php-oxygen.com>
+ * @author      Sébastien HEYD <sheyd@php-oxygen.com>
+ * @package     PHP Oxygen
+ */
+
+$platforms = array(
+    "windows nt 6.2" => "Windows 8",
+    "windows nt 6.1" => "Windows 7",
+    "windows nt 6.0" => "Windows Vista",
+    "windows nt 5.2" => "Windows 2003",
+    "windows nt 5.1" => "Windows XP",
+    "windows nt 5.0" => "Windows 2000",
+    "windows nt 4.0" => "Windows NT 4.0",
+    "winnt4.0" => "Windows NT 4.0",
+    "winnt 4.0" => "Windows NT 4.0",
+    "winnt" => "Windows NT",
+    "windows 98" => "Windows 98",
+    "win98" => "Windows 98",
+    "windows 95" => "Windows 95",
+    "win95" => "Windows 95",
+    "windows ce" => "Windows Mobile",
+    "windows phone" => "Windows Mobile",
+    "windows" => "Unknown Windows OS",
+    "os x" => "Mac OS X",
+    "ppc mac" => "Power PC Mac",
+    "freebsd" => "FreeBSD",
+    "ppc" => "Macintosh",
+    "gentoo" => "Gentoo",
+    "ubuntu" => "Ubuntu",
+    "debian" => "Debian",
+    "linux" => "Linux",
+    "sunos" => "Sun Solaris",
+    "beos" => "BeOS",
+    "apachebench" => "ApacheBench",
+    "aix" => "AIX",
+    "irix" => "Irix",
+    "osf" => "DEC OSF",
+    "hp-ux" => "HP-UX",
+    "netbsd" => "NetBSD",
+    "bsdi" => "BSDi",
+    "openbsd" => "OpenBSD",
+    "gnu" => "GNU/Linux",
+    "unix" => "Unknown Unix OS",
+    "ipod" => "iPod",
+    "wii" => "Nintendo Wii",
+    "playStation portable" => "PlayStation Portable"
+);
