46311702c74eb19632c0e7f92121e7a83654c954
sebastien.heyd@gmail.com <sebastien.heyd@gmail.com@063b2e71-347a-c81b-b46b-1f68757473af>
2012-07-31 10:12:55 +0000

    Copyright notice 2011-2012

--- trunk/oxygen/lib/useragent.class.php
+++ trunk/oxygen/lib/useragent.class.php
@@ -6,7 +6,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @copyright   Copyright (c) 2011 Sébastien HEYD <sheyd@php-oxygen.com>
+ * @copyright   Copyright (c) 2011-2012 Sébastien HEYD <sheyd@php-oxygen.com>
  * @author      Sébastien HEYD <sheyd@php-oxygen.com>
  * @package     PHP Oxygen
  */
