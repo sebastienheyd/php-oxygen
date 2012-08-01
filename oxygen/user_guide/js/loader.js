5673fb73d7ab257bf5374691964b5e42b65e2b97
sebastien.heyd@gmail.com <sebastien.heyd@gmail.com@063b2e71-347a-c81b-b46b-1f68757473af>
2012-08-01 14:00:30 +0000

    guide brush for Smarty tags + french documentation

--- oxygen/user_guide/js/loader.js
+++ oxygen/user_guide/js/loader.js
@@ -6,6 +6,7 @@ var myScripts = new Array(
     basepath+'js/shBrushCss.js', 
     basepath+'js/shBrushSql.js', 
     basepath+'js/shBrushXml.js', 
+    basepath+'js/shBrushSmarty.js', 
     basepath + lang+'/variables.js', 
     basepath+'js/user_guide.js');
  
