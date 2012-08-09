66cb32b88c24498e72ab275cf1dccf2e771a6069
sebastienheyd@gmail.com <sebastienheyd@gmail.com@063b2e71-347a-c81b-b46b-1f68757473af>
2012-08-09 18:28:54 +0000

    session fixation

--- oxygen/core/session.class.php
+++ oxygen/core/session.class.php
@@ -10,53 +10,52 @@
  * @author      Sébastien HEYD <sheyd@php-oxygen.com>
  * @package     PHP Oxygen
  */
-
 class Session
 {
-	private static $_instance;
 
+    private static $_instance;
     private static $_handlers = array('database', 'files', 'cookie', 'memcached');
-    
-    private $_sessionActive = false;
+    private static $_sessionActive = false;
+    private static $_data;
 
-	/**
-	 * Get instance of Session
-	 *
-	 * @return Session      Return singleton instance of Session
-	 */
-	public static function getInstance()
-	{
-		if(!isset(self::$_instance)) self::$_instance = new self();
-		return self::$_instance;
-	}
-    
-	/**
-	 * @return Session
-	 */
-	private function __construct()
-	{
+    /**
+     * Get instance of Session
+     *
+     * @return Session      Return singleton instance of Session
+     */
+    public static function init()
+    {
+        if (!isset(self::$_instance))
+            self::$_instance = new self();
+        return self::$_instance;
+    }
+
+    /**
+     * @return Session
+     */
+    private function __construct()
+    {
         $handler = Config::get('session.handler');
-        
-        if(in_array($handler, self::$_handlers))
+
+        if (in_array($handler, self::$_handlers))
         {
-            $cname = 'f_session_'.ucfirst(strtolower($handler));
-            
-            $handler = new $cname(Config::get('session.maxlifetime',get_cfg_var("session.gc_maxlifetime")));
-            
+            $cname = 'f_session_' . ucfirst(strtolower($handler));
+
+            $handler = new $cname(Config::get('session.maxlifetime', get_cfg_var("session.gc_maxlifetime")));
+
             session_set_save_handler(
-                array($handler, "open"),
-                array($handler, "close"),
-                array($handler, "read"),
-                array($handler, "write"),
-                array($handler, "destroy"),
-                array($handler, "gc")
+                    array($handler, "open"), 
+                    array($handler, "close"), 
+                    array($handler, "read"), 
+                    array($handler, "write"), 
+                    array($handler, "destroy"), 
+                    array($handler, "gc")
             );
-        }                
-        
-        $this->start();
-	}
-    
-    
+        }
+
+        self::_start();            
+    }
+
     /**
      * Shortcut to set var in session
      * 
@@ -66,7 +65,7 @@ class Session
      */
     public static function set($name, $value)
     {
-        $_SESSION[$name] = $value;
+        self::$_data[$name] = $value;
     }
 
     /**
@@ -78,182 +77,88 @@ class Session
      */
     public static function get($name, $defaultValue = false)
     {
-        return isset($_SESSION[$name]) ? $_SESSION[$name] : $defaultValue;
+        return isset(self::$_data[$name]) ? self::$_data[$name] : $defaultValue;
     }
-    
+
     /**
      * Shortcut to unset value from session
      * 
      * @param string $name      Variable name to delete from session
      * @return void
-     */    
+     */
     public static function delete($name)
     {
-         self::getInstance()->__unset($name);
+        if(isset(self::$_data[$name])) unset(self::$_data[$name]);
     }
-    
+
     /**
      * Start a new session
      *
      * @return boolean      Return true if session is started
      */
-    public function start()
+    private static function _start()
     {
-        if(!$this->_sessionActive) $this->_sessionActive = session_start();
-        return $this->_sessionActive;
+        if(!self::$_sessionActive)
+        {
+            session_name('O2_SESSION');
+            self::$_sessionActive = session_start();
+            self::$_data = $_SESSION;
+            $_SESSION = array();
+            session_regenerate_id(true);
+        }
+        return self::$_sessionActive;
     }
 
-	/**
-	 * Retrieve the current session id
-	 *
-	 * @return string       The current session id
-	 */
-	public function getId()
-	{
-		return session_id();
-	}
+    /**
+     * Retrieve the current session id
+     *
+     * @return string       The current session id
+     */
+    public static function getId()
+    {
+        return session_id();
+    }
 
     /**
-     * Magic method used when writing data to inaccessible properties.
-     * If value is null, the variable will be unset
-     *
-     * @see    http://www.php.net/manual/en/language.oop5.overloading.php
-     * @example f_Session::getInstance()->mydata = 'my value' (will set $_SESSION['mydata])
+     * Free all session variables
      * 
-     * @param string $name  Name of the variable to write into session
-     * @param mixed $value  Value of the variable to insert
      * @return void
      */
-    public function __set($name, $value)
+    public static function clean()
     {
-        if($value === null)
-        {           
-            if(isset($_SESSION[$name])) unset($_SESSION[$name]);
-        }
-        else
-        {
-            $_SESSION[$name] = $value;
-        }
+        self::$_data = array();
     }
 
     /**
-     * Magic method used for reading data from inaccessible properties.
-     * Equivalent to $_SESSION[$name]
-     *
-     * @link    http://www.php.net/manual/en/language.oop5.overloading.php
-     * @example f_Session::getInstance()->mydata : will return $_SESSION['mydata']
-     * 
-     * @param string $name      Name of the property to get from the current session
-     * @return  mixed|null      Return value of the property else null
-     */
-    public function __get($name)
-    {
-        return isset($_SESSION[$name]) ? $_SESSION[$name] : null;
-    }
-
-    /**
-     * Magic method invoked by calling isset() or empty() on inaccessible properties.
-     * Equivalent to isset($_SESSION[$name])
-     *
-     * @link    http://www.php.net/manual/en/language.oop5.overloading.php
-     * @param   string $name        Name of the variable to check from the current session
-     * @return  boolean             Return true if variable exist in the current session
-     */
-    public function __isset($name)
-    {
-        return isset($_SESSION[$name]);
-    }
-
-    /**
-     * Magic method invoked when unset() is used on inaccessible properties.
-     * Equivalent to unset($_SESSION[$name])
-     *
-     * @link    http://www.php.net/manual/en/language.oop5.overloading.php
-     * @param   string $name        Name of the variable to remove from the current session
-     */
-    public function  __unset($name)
-    {
-        if(isset($_SESSION[$name])) unset($_SESSION[$name]);
-    }
-
-    /**
-     * Magic method invoked when function does not exists in Session.
-     * Works with set, get or unset functions
-     *
-     * @example Session:getInstance()->setMyVar('my value') : will set $_SESSION['myVar']
-     * @example Session:getInstance()->getMyVar() : will get $_SESSION['myVar']
-     * @example Session:getInstance()->unsetMyVar() : will unset $_SESSION['myVar']
-     *
-     * @param   string $method        Name of the called method must begin with get, set or unset
-     * @param   array $argument       Arguments of the called method
-     * @return  Session               Return current Session instance
-     */
-    public function __call($method, $argument)
-    {
-        if(preg_match('/^(set|get|unset)([A-Za-z0-9]?)$/', $method, $matches))
-        {
-            $name = lcfirst($matches[2]);
-            switch ($matches[1])
-            {
-                case 'get':
-                   if(isset($argument[0]))
-                   return $this->$name;
-                break;
-
-                case 'set':
-                    $this->$name = $argument[0];
-                    return $this;
-                break;
-
-                case 'unset':
-                    $this->$name = null;
-                    return $this;
-                break;
-            
-                default:
-                    throw new BadMethodCallException('Method '.$method.' does not exist');
-                break;
-            }
-        }
-
-        return null;
-    }
-    
-    /**
-     * When destroyed, close session
-     */
-    public function __destruct()
-    {
-        session_write_close();
-    }     
-
-	/**
-	 * Free all session variables
-     * 
-     * @return void
-	 */
-	public function clean()
-	{
-		session_unset();
-	}
-
-	/**
-	 * Destroy the current session and start a new one
+     * Destroy the current session and start a new one
      * 
      * @return boolean  Return true if session is detroyed
-	 */
-	public function destroy()
-	{
-		if($this->_sessionActive)
+     */
+    public static function destroy()
+    {
+        if (self::$_sessionActive)
         {
-            session_destroy();            
-            unset($_SESSION);
-            $this->_sessionActive = false;
-            $this->start();
-            session_regenerate_id();            
-            return true;
+            $params = session_get_cookie_params();
+            setcookie(session_name(), '', time() - 42000,
+                $params["path"], $params["domain"],
+                $params["secure"], $params["httponly"]
+            );
+
+            session_destroy();
+            self::$_data = array();
+            self::$_sessionActive = false;
+            return self::_start();
         }
 
         return false;
-	}
+    }
+    
+    /**
+     * When destroyed, write in session and close it
+     */
+    public function __destruct()
+    {
+        $_SESSION = self::$_data;
+        session_write_close();
+    }    
 }
