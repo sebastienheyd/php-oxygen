9eb49e03a3bb98812b26c53f25ad773de6ecdd0a
sebastien.heyd@gmail.com <sebastien.heyd@gmail.com@063b2e71-347a-c81b-b46b-1f68757473af>
2013-08-19 13:18:48 +0000

    fix user agent detection + fix tablet PC detection

--- oxygen/lib/data/devices.php
+++ oxygen/lib/data/devices.php
@@ -1,17 +1,17 @@
 <?php
 
 $devices = array(
-    "kindle|mobile|mmp|midp|pocket|psp|symbian|symbos|smartphone|treo|up.browser|up.link|vodafone|wap|opera mini" => array("type" => "phone", "value" => "generic"),
-    "tablet" => array("type" => "tablet", "value" => "generic"),
     "iphone|ipod" => array("type" => "phone", "value" => "iphone"),
     "android.*mobile" => array("type" => "phone", "value" => "android"),
-    "blackberry" => array("type" => "phone", "value" => "blackberry"),
-    "samsung|bada" => array("type" => "phone", "value" => "samsung"),
-    "meego|nokia" => array("type" => "phone", "value" => "nokia"),
-    "avantgo|blazer|elaine|hiptop|palm|plucker|xiino" => array("type" => "phone", "value" => "palm"),
-    "windows ce; (iemobile|ppc|smartphone)" => array("type" => "phone", "value" => "windows"),
-    "windows phone os" => array("type" => "phone", "value" => "windowsphone"),
     "ipad" => array("type" => "tablet", "value" => "ipad"),
     "android(?!.*mobile)" => array("type" => "tablet", "value" => "androidtablet"),
-    "rim tablet os" => array("type" => "tablet", "value" => "blackberrytablet")
+    "windows phone os" => array("type" => "phone", "value" => "windowsphone"),
+    "blackberry" => array("type" => "phone", "value" => "blackberry"),
+    "samsung|bada" => array("type" => "phone", "value" => "samsung"),
+    "avantgo|blazer|elaine|hiptop|palm|plucker|xiino" => array("type" => "phone", "value" => "palm"),
+    "windows ce; (iemobile|ppc|smartphone)" => array("type" => "phone", "value" => "windows"),
+    "rim tablet os" => array("type" => "tablet", "value" => "blackberrytablet"),
+    "meego|nokia" => array("type" => "phone", "value" => "nokia"),
+    "tablet(?!.*PC)" => array("type" => "tablet", "value" => "generic"),
+    "kindle|mobile|mmp|midp|pocket|psp|symbian|symbos|smartphone|treo|up.browser|up.link|vodafone|wap|opera mini" => array("type" => "phone", "value" => "generic")
 );
