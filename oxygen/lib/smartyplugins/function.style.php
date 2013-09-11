08bb32a0f0cb2302e0b654ef86434a21bbc36f0a
sebastienheyd@gmail.com <sebastienheyd@gmail.com@063b2e71-347a-c81b-b46b-1f68757473af>
2013-09-11 16:23:16 +0000

    new css minifier

--- oxygen/lib/smartyplugins/function.style.php
+++ oxygen/lib/smartyplugins/function.style.php
@@ -10,17 +10,20 @@ function smarty_function_style($params, &$smarty)
 {
     $files = explode(',', $params['href']);    
     if(empty($files)) return '';
+    
+    $dir = '';
+    if(isset($params['dir'])) $dir = rtrim($params['dir'], '/').'/';
 
     if(Config::get('asset.combine', true) === false)
     {
-        foreach($files as $file) echo '<link rel="stylesheet" type="text/css" href="'.$file.'" />';
+        foreach($files as $file) echo '<link rel="stylesheet" type="text/css" href="'.$dir.$file.'" />'.PHP_EOL;
     }
     else
     {
         $asset = Asset::getInstance();
-        foreach($files as $file) $asset->add($file);
+        foreach($files as $file) $asset->add($dir.$file);
         $asset->compile();
 
-        echo '<link rel="stylesheet" type="text/css" href="/'.$asset->getUid().'.css" />';        
+        echo '<link rel="stylesheet" type="text/css" href="/'.$asset->getUid().'.css" />'.PHP_EOL;        
     }
 }
