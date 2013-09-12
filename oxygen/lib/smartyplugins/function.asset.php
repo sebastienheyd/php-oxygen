9fa3c68ed0a48bd9f22558abe9ecf96bd28a0b7f
sebastien.heyd@gmail.com <sebastien.heyd@gmail.com@063b2e71-347a-c81b-b46b-1f68757473af>
2013-09-12 14:26:40 +0000

    added relative to absolute path fix for css combination - renamed smarty function style to asset

---
+++ oxygen/lib/smartyplugins/function.asset.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Add an html tag for the given asset(s)
+ * 
+ * @param array $params
+ * @param Template $smarty
+ * @return string
+ */
+function smarty_function_asset($params, &$smarty)
+{
+    if(!isset($params['href'])) throw new SmartyException('No parameter href is set');
+    
+    // Getting files list
+    $files = explode(',', $params['href']);
+    if(empty($files)) throw new SmartyException('href parameter is empty');
+    
+    $dir = '';
+    if(isset($params['dir'])) $dir = rtrim($params['dir'], '/').'/';
+    
+    // Getting file type
+    if(!isset($params['type']))
+    {
+        $ext = substr($files[0], strrpos($files[0], '.') + 1);
+        switch($ext)
+        {
+            case 'less':
+            case 'css':
+                $type = 'css';
+                break;
+            
+            case 'js':
+                $type = 'js';
+                break;
+        }
+    }
+    else
+    {
+        $type = $params['type'];
+    }
+    
+    if(!isset($type) || ($type !== 'js' && $type !== 'css') )
+        throw new SmartyException('Type is not defined');
+
+    if(Config::get('asset.combine', true) === false || count($files) === 1) // combining is off
+    {
+        if($type === 'css')
+        {
+            foreach($files as $file) 
+                echo '<link rel="stylesheet" type="text/css" href="'.$dir.$file.'" />'.PHP_EOL;            
+        }
+    }
+    else  // combining is on
+    {
+        $asset = Asset::getInstance();
+        foreach($files as $file) $asset->add($dir.$file);
+        $asset->compile();
+        
+        if($type === 'css')
+        {
+            echo '<link rel="stylesheet" type="text/css" href="/'.$asset->getUid().'.css" />'.PHP_EOL;
+        }
+    }
+}
