f819033f9de3eae0f83b43668fb6a23c9434d265
sebastien.heyd@gmail.com <sebastien.heyd@gmail.com@063b2e71-347a-c81b-b46b-1f68757473af>
2013-10-02 10:12:46 +0000

    Added AjaxAction to secure a little bit ajax action if needed

---
+++ trunk/oxygen/core/ajaxaction.class.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This file is part of the PHP Oxygen package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright   Copyright (c) 2011-2012 Sébastien HEYD <sheyd@php-oxygen.com>
+ * @author      Sébastien HEYD <sheyd@php-oxygen.com>
+ * @package     PHP Oxygen
+ */
+abstract class AjaxAction extends Action
+{
+    public function isAuthorized()
+    {
+        // Check if we have an XMLHttpRequest context
+        if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) !== 'xmlhttprequest') 
+            return false;
+        
+        // Check if we call from the same referer
+        if(!isset($_SERVER['HTTP_REFERER']) || !preg_match('#^https?://'.$_SERVER['HTTP_HOST']."#", $_SERVER['HTTP_REFERER']))
+            return false;
+        
+        return true;
+    }
+    
+    public function errorHandler() 
+    {
+        Error::show401();
+    }
+}
